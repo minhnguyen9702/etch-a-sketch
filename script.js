b82c727810544bef070238cd5ab6ad2dfa3aabf3
minhnguyen9702 <minhnguyen9702@gmail.com>
2022-12-08 20:48:45 +1100

    setup grid

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-function createGrid (gridSize) {
-    for
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        container.appendChild(createRow(gridSize));
+    }
 }
+
+function createRow(gridSize) {
+    const row = document.createElement('div')
+    for (let i = 0; i < gridSize; i++) {
+        row.appendChild(createCell(gridSize));
+        console.log(row)
+    }
+    return row
+}
+
+function createCell(gridSize) {
+    const cell = document.createElement('div');
+    cell.setAttribute('class', 'cell')
+    cell.setAttribute('style', 
+    `width: ${800 / gridSize}px; 
+    height: ${800 / gridSize}px; 
+    background: white;`);
+    return cell
+}
+
+const container = document.querySelector('.container')
+container.appendChild((createGrid(16)))
